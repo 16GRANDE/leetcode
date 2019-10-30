eee20dafeebae3d597dcda751c346d95a3685255
16GRANDE <syk20001000@gmail.com>
2019-10-30 19:05:43 +0800

    add 21.合并两个有序链表.js

--- leetcode answer/1143.最长公共子序列.js
+++ leetcode answer/1143.最长公共子序列.js
@@ -11,21 +11,21 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    let len1=text1.length,len2=text2.length
-    let result=new Array(len1+1)
-    for(let i=0;i<=len1;i++){
-        result[i]=new Array(len2+1).fill(0)
+  let len1 = text1.length,
+    len2 = text2.length;
+  let result = new Array(len1 + 1);
+  for (let i = 0; i <= len1; i++) {
+    result[i] = new Array(len2 + 1).fill(0);
+  }
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        result[i][j] = result[i - 1][j - 1] + 1;
+      } else {
+        result[i][j] = Math.max(result[i - 1][j], result[i][j - 1]);
+      }
     }
-    for(let i=1;i<=len1;i++){
-        for(let j=1;j<=len2;j++){
-            if(text1[i-1]===text2[j-1]){
-                result[i][j]=result[i-1][j-1]+1
-            }else{
-                result[i][j]=Math.max(result[i-1][j],result[i][j-1])
-            }
-        }
-    }
-    return result[len1][len2]
+  }
+  return result[len1][len2];
 };
 // @lc code=end
-
